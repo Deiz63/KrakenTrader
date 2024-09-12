091bf63430a41a12161cc7e26238c0be6000d74b
Deiz63 <104404214+Deiz63@users.noreply.github.com>
2024-09-12 08:08:33 -0400

    Update KrakenCancelOrdersForToken.py
    Moved take_profit & stop_loss calcs to Main. Revision to bought_price logic.

--- KrakenCancelOrdersForToken.py
+++ KrakenCancelOrdersForToken.py
@@ -3,56 +3,42 @@ from kraken.spot import User, Trade, Market
 from colorama import Fore, Style
 from time import sleep
 # TODO: check rounding & (decimals) for all lines, verify correct
-# TODO: Need to get correct Buy vs Low_10 vs purchased, 
-# set edit thresholds - create variables?
-# Fix edit order for Stop-limit, do I need to cancel & create new?
+# TODO: Double-Check Buy vs Low_10 vs purchased, vs BOUGHT -  
+# set edit thresholds - create variables? (TL vs SL) : .75 tp or sl delta - 
+
 bought_price = None 
 
-token = 'CQT'               # TODO: 
-buy_price = .0045              # TODO: Enter Desired Buy Price
-# Above is the initial desired buy price.
-# Once an order is placed, the actual order price will be used. 
-# TODO: CHECK IF THE FOLLOWING LINE IS WORKING, edit & change location if not successful. 
-if bought_price:
-    buy_price = bought_price    # TODO: When buy order placed, buy_price will use that order_price.                       
-    print(f"Buy order was placed, buy_price = {bought_price}")
-        
+token = 'CXT'               # TODO: 
+buy_price = .06675              # TODO: Enter Desired Initial Buy Price
+
 stop_loss_value = .10         # TODO: slv * 100 = Sl percent
 take_profit_value = .015       # TODO: tpv * 100 = TP percent
+min_order_multplier = 5         # TODO: Purchase amount - min_order * momo
 
 # Gather token data
 token_pair = f'{token}USD'
 
+# Get Token Info Data - (used for orders decimal rounding, order mins, etc.)
 def get_asset_data(token_pair):
     print('Get Asset Pair details, decimals, min order, etc.')
     data = Market().get_asset_pairs(pair=token_pair)
-    # Check decimals pair vs. cost ???
     pair_decimals = data[token_pair]['pair_decimals']   # 0 - for prices in this pair
     cost_decimals = data[token_pair]['cost_decimals']   # 1 - cost of trades in pair (quote asset terms)
     lot_decimals = data[token_pair]['lot_decimals']     # 2 - volume (base asset terms)
     ordermin = data[token_pair]['ordermin']             # 3 - Min order size
     return pair_decimals, cost_decimals, lot_decimals, ordermin
 
+# Calculations - get Asset data: Volume & Price max decimals allowed 
 asset_data = get_asset_data(token_pair)
-# Calculations - Price / Volume
 order_min = float(asset_data[3])                # Decimals for Order Min
-order_calc = 3 * order_min
-order_volume = order_min + order_calc       # NOTE: Order Vol = Min + 25%
-
+order_calc = min_order_multplier * order_min
+order_volume = order_calc       # NOTE: Order Vol = Min + 25%
 price_round_value = asset_data[0]           # Decimals for Buy Price
 volume_round_value = asset_data[2]          # Decimals for Volume
 
-stop_loss_delta = (buy_price * stop_loss_value)
-stop_loss = round(buy_price - stop_loss_delta, price_round_value)
-take_profit_delta = (buy_price * take_profit_value)
-take_profit_add = (buy_price + take_profit_delta)
-take_profit = round(take_profit_add, price_round_value)
-volume_calc = 10 / buy_price
-volume_to_order = round((volume_calc), volume_round_value)
 
-trigger_price = round(stop_loss - (.25 * stop_loss_delta), price_round_value)
-limit_price = round(stop_loss, price_round_value)
-
+#volume_calc = 10 / buy_price       # Can be used to provide $$ value for Buy order Qt.  
+#volume_to_order = round((volume_calc), volume_round_value)
 
 def check_balance(token):     # check_balance
     user = User(key=api_key, secret=secret_key)
@@ -69,7 +55,6 @@ def get_existing_balance(token):
         existing_balance = False
     print(Fore.GREEN + f'Current Token Balance = {token_balance}, Existing Balance = {existing_balance}' + Style.RESET_ALL)
     return existing_balance
-
 
 def get_ticker_data(token_pair):
     ticker_data = Market().get_ticker(pair=token_pair)
@@ -99,12 +84,12 @@ def id_matching_order(token_pair, orders):
         order_price = order_details['descr']['price']
         # Check if any orders match token pair.
         if asset_pair == token_pair:
-            print(f"ID matching order token pair = asset pair : {token_pair} = {asset_pair}")
+            #print(f"ID matching order token pair = asset pair : {token_pair} = {asset_pair}")
             matched_order = order_id
             matching_order = True
             #print(f'asset_pair = {asset_pair} token_pair = {token_pair}.')
-            print(f'Open order for {token_pair} found : Order Action = {order_action}, Order Price = {order_price}')
-            print(Fore.YELLOW + f'order ID = {order_id}.' + Style.RESET_ALL) 
+            print(Fore.YELLOW + f'Order ID = {order_id} found for {token_pair}: Order Action = {order_action}, Order Price = {order_price}'+ Style.RESET_ALL)
+            #print(Fore.YELLOW + f'order ID = {order_id}.' + Style.RESET_ALL) 
         else:
             pass
     return matching_order
@@ -165,8 +150,7 @@ def edit_open_order(token_pair, orders, edit_price):
     for order_id, order_details in orders['open'].items():
         asset_pair = order_details['descr']['pair']
         if asset_pair == token_pair:
-            print(Fore.RED + f'Revising Open order ID = {order_id}, Token {token_pair}.' + Style.RESET_ALL)
-            print('Need to fix this process, check prices vs order to avoid constant failed edit attempts')
+            print(Fore.RED + f'Revising Order ID {order_id}, for Token: {token_pair}.' + Style.RESET_ALL)
             # Initialize the Trade client with your API key and secret
             trade = Trade(key=api_key, secret=secret_key)
             # Edit an existing order
@@ -211,14 +195,14 @@ def buy_place(low_10_latest, token_pair):
     sleep(60)
     return bought_price
     
-def sell_check(current_price, orders, buy_price, take_profit_delta, token_balance):
+def sell_check(trigger_price, limit_price, current_price, orders, buy_price, token_balance, stop_loss, stop_loss_delta, take_profit, take_profit_delta):
     # Edit this to use the Actual buy price 
     # buy_price is hardcoded, low_10 will change. How to confirm price?
     # Edit thresholds will trigger edits at 75% of SL or TP goals. 
     order_price = get_order_price(token_pair, orders)
-    tp_edit_delta = 0.75 * take_profit_delta
+    tp_edit_delta = 0.5 * take_profit_delta
     tp_threshold = buy_price + tp_edit_delta
-    sl_edit_delta = 0.75 * stop_loss_delta
+    sl_edit_delta = 0.9 * stop_loss_delta
     sl_threshold = buy_price - sl_edit_delta
     print(Fore.YELLOW + 'Run Sell-Check Function : Check Sell Price, type = Sell' + Style.RESET_ALL)
     print(f'Check order price vs Current. Edit if current < 75%-SL @ {sl_threshold} OR  > 75%-TP @ {tp_threshold}') 
@@ -232,7 +216,7 @@ def sell_check(current_price, orders, buy_price, take_profit_delta, token_balanc
             create_take_profit_order(take_profit, token_pair, token_balance)
             print(Fore.GREEN + f'New TAKE PROFIT ORDER Created - Take Profit @ {take_profit}' + Style.RESET_ALL)   
     if current_price < sl_threshold:
-        print(Fore.CYAN + f'SL Threshold Reached @ {sl_threshold} Current = {current_price}.Assure that STOP LOSS price is on ORDER' + Style.RESET_ALL)
+        print(Fore.CYAN + f'SL Threshold Reached @ {sl_threshold} Current Price = {current_price}.Assure that STOP LOSS price is on ORDER' + Style.RESET_ALL)
         edit_price = stop_loss
         print(f'order_price = {order_price}, stop_loss = {stop_loss}, trigger_price = {trigger_price}')
         if order_price == stop_loss or order_price == trigger_price:
